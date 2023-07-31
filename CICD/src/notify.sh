caa9bbe98775b7cf67f9b5b7e78cda695c345fb7
KolyaDavydov <100277134+KolyaDavydov@users.noreply.github.com>
2023-07-31 15:29:18 +0300

    Update notify.sh

--- CICD/src/notify.sh
+++ CICD/src/notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TELEGRAM_BOT_TOKEN="6351659682:AAEtm5RRq7Ck7kwYWvo6m3BiKH2KCVl_txE"
-TELEGRAM_USER_ID="156215020"
+TELEGRAM_BOT_TOKEN="Некоторый токен"
+TELEGRAM_USER_ID="Некоторое ID"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Статус: $1%0A%0AПроект:+$CI_PROJECT_NAME%0AСтатус:+$CI_JOB_STATUS%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0AВетка:+$CI_COMMIT_REF_SLUG"
 
